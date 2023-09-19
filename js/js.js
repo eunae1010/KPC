2d73cce37ca8c1d292e0a21ddd20ab8abe0405e5
eunae1010 <cea1010@naver.com>
2023-09-19 17:41:19 +0900

    첫 업로드

---
+++ js/js.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".swiper", {
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
